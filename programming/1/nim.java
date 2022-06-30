0fafafe651a9ced42adff9a01b6644fe1b0754af
Christine Marra <96950263+chrismarra21@users.noreply.github.com>
2022-06-30 14:22:17 +0000

    first upload

---
+++ programming/1/nim.java
@@ -0,0 +1,36 @@
+import java.i0.*;
+import java.util.*;
+
+public class nim
+  {
+
+   public static void main (String[] args)
+    {
+        int stones = 12;
+        int stonesTaken;
+
+        Scanner input = new Scanner(System.in);.println
+        
+
+        while(stones>0){
+          System.out.print ("there are %d stones left in the bag \n", stones);
+          Sytem .out.print ("How many stones do you want to pick?");
+          stonesTaken = input.nextInt();
+
+          stones = stones-stonesTaken;
+          System.out.println
+
+
+
+
+
+
+
+
+
+
+
+          
+        }
+    }
+  }
