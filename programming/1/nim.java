17966a6514e0762c4ca27ba94c8faee4f2c0c077
Christine Marra <96950263+chrismarra21@users.noreply.github.com>
2022-07-01 16:57:48 +0000

    first submit

--- programming/1/nim.java
+++ programming/1/nim.java
@@ -1,5 +1,10 @@
+
+//Game of Nim
+//Christine Marra
+//David and Nicole
 import java.i0.*;
 import java.util.*;
+import random;
 
 public class nim
   {
@@ -9,16 +14,23 @@ public class nim
         int stones = 12;
         int stonesTaken;
 
-        Scanner input = new Scanner(System.in);.println
+        Scanner input = new           
+        Scanner(System.in);.println
         
 
         while(stones>0){
           System.out.print ("there are %d stones left in the bag \n", stones);
-          Sytem .out.print ("How many stones do you want to pick?");
+          System.out.print ("How many stones do you want to pick?");
           stonesTaken = input.nextInt();
 
           stones = stones-stonesTaken;
-          System.out.println
+          System.out.printf("there are %d stones remaining. \n", stones);
+
+          if stones <3){
+            stonesTaken = rand.nextInt(stones+1) +1;
+          } else{
+            
+          }
 
 
 
