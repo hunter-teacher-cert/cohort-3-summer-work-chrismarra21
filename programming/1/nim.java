43230ebd8b64da1fb36b33ee3279b33afdb9fe94
Christine Marra <96950263+chrismarra21@users.noreply.github.com>
2022-07-11 01:47:23 +0000

    correcting a looping problem

--- programming/1/nim.java
+++ programming/1/nim.java
@@ -1,11 +1,12 @@
 
 //Game of Nim
 //Christine Marra
-//Collaborators David and Nicole 
-//Java Hungry by Subham Mittal
-import java.i0.*;
+//Collaborators David M. and Nicole C.
+// Help from Wayne Tobias
+
+import java.io.*;
 import java.util.*;
-import random;
+
 
 public class nim
   {
@@ -17,39 +18,45 @@ public class nim
 
         Scanner rock = new Scanner(System.in);
         while(stones>0){
-          System.out.print ("There are " + stones  + " stones left in the bag \n");
+          System.out.print ("There are " + stones  + " stones in the bag.\n");
           System.out.print ("How many stones (1, 2, or 3) do you want to pick?");
           stonesTaken = rock.nextInt();
+            if (stonesTaken >3 || stonesTaken>stones){
+              System.out.println("You took too many, pick again.")
+              System.out.print ("How many stones (1, 2, or 3) do you want to pick?");
+              stonesTaken = rock.nextInt();
+            }else{
+              stonesTaken = rock.nextInt();
+            }
 
           stones = stones-stonesTaken;
           System.out.println("You took " + stonesTaken +  " stones. \n");
           System.out.println("There are " + stones +  " stones in the bag. \n");
           if (stones==0){
             System.out.println("You took the last stone, WINNER!!");
+            break;
           }
-          else {
+       
             if (stones >3){
               Random computerTake = new Random();
               stonesTaken = computerTake.nextInt(3) +1;
-              {else}
-                stonesTaken = 3;
-          } 
+            }
+              else{
+                stonesTaken = stones;
+              }
+                  
+                
+            } 
 
           stones = stones-stonesTaken;
           System.out.println("The Computer took " + stonesTaken +  " stones. \n");
           System.out.println("There are " + stones +  " stones in the bag. \n");
           if (stones==0){
             System.out.println("The computer took the last stone, You lost.");
+            break;
           }
 
-
-
-
-
-
-
-
-          
+         
         }
     }
   }
