51e2311897cc616b8943a11a38e28b0741ac297f
Christine Marra <96950263+chrismarra21@users.noreply.github.com>
2022-07-15 17:11:03 +0000

    Added pre_06 FOR LOOP lesson plan

---
+++ ds/LinkedLists/Driver.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+  public static void main(String[] args) {
+    LinkedList l = new LinkedList();
+    l.add("one");
+    l.add("two");
+    System.out.println(l);
+    
+    System.out.println(lget(1));
+  }
+}
---
+++ methods/pp.java
@@ -0,0 +1,26 @@
+import io.util*;
+
+public class pp 
+{
+  public static String barGraphify(int[] a)
+  {
+    String retStr = "";
+    for (int row=0; row<a.length; row++ ) {
+      //create variable, compare to integer, and everytime it loops it will create an equal sign.
+      
+      System.out.println()
+        = numbers* "=" 
+      retStr += "\n";
+    }
+    return retStr;
+
+
+  }    
+}
+
+// int[] numbers = new int [4];
+//numbers [0]= 5;
+//numbers [1]= 1;
+//numbers [2]= 8;
+//numbers [3]= 2;
+  
